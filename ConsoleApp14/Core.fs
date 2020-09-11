b8651102b7a3cac20e8e3c278f575427d67288b6
Radu Popovici <rpopovici@totalsoft.ro>
2020-09-11 13:37:10 +0300

    poc

---
+++ ConsoleApp14/Core.fs
@@ -0,0 +1,51 @@
+﻿module Core
+
+open NBB.Core.Effects.FSharp
+
+type Token<'a> = Ctx -> Effect<'a>
+    and Ctx = Ctx of int
+
+[<AutoOpen>]
+module Token =
+    let map (func: 'a -> 'b) (eff: Token<'a>) : Token<'b> = 
+        fun ctx ->
+            eff ctx |> Effect.map func
+
+    let bind (func: 'a -> Token<'b>) (eff: Token<'a>): Token<'b> = 
+        fun ctx ->
+            eff ctx |> Effect.bind (fun a -> func a ctx)
+
+    let apply (func:Token<'a->'b>) (eff:Token<'a>) = 
+        bind (fun a -> func |> map (fun fn -> fn a)) eff
+
+    let return' (x:'a): Token<'a> =
+        fun _ctx -> Effect.return' x
+
+    let composeK f g x = bind g (f x)
+    
+    let lift2 f = map f >> apply
+    
+    let flatten eff = bind id eff
+
+module Sql = 
+    let from<'a> table column type' = 
+        fun ctx -> effect { return "Radu Popovici" }
+
+module Rest = 
+    let from<'a> url method parameter type' = 
+        fun ctx -> effect { return 253 }
+
+let cache token  = token
+
+let trace token = 
+    fun (Ctx id) -> 
+    effect{
+        printfn "some tracing"
+        return! token (Ctx id)
+    }
+
+
+
+
+
+
---
+++ ConsoleApp14/Program.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Core
+open NBB.Core.Effects.FSharp
+
+
+let main' = 
+    
+    let angajat = Sql.from "table" "column" "string" |> cache |> trace
+    let marca = Rest.from "www.google.com" "POST" "id" "int" |> cache |> trace
+    
+    let template = "Salut {angajat}, Bine ai venit in companie, marca ta este {marca}."
+    //let templateFn = parse template
+    let templateFn = sprintf "Salut %s, Bine ai venit in companie, marca ta este %i."
+    let liftedTemplateFn = lift2 templateFn
+    let resultToken = liftedTemplateFn angajat marca
+
+    let ctx = Ctx 1666
+    resultToken ctx
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let interpreter = Interpreter.createInterpreter()
+    let x = 
+        main' 
+        |> Effect.interpret interpreter
+        |> Async.RunSynchronously
+    printfn "%s" x
+    0 // return an integer exit code
